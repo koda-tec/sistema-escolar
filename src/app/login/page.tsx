040eab65da8dda975449777adeb3b6183aa61b56
GonzaloBouso <gonzalobouso002@gmail.com>
2026-02-20 01:33:18 -0300

    add link en login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react'
 import { createClient } from '@/app/utils/supabase/client'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 
 export default function LoginPage() {
   const [email, setEmail] = useState('')
@@ -77,6 +78,17 @@ export default function LoginPage() {
               {loading ? 'Validando...' : 'Iniciar Sesión'}
             </button>
           </form>
+          <div className="mt-4 text-center text-sm text-gray-600">
+          <p>
+            ¿No tienes cuenta?{' '}
+            <Link 
+              href="/register" 
+              className="font-medium text-blue-600 hover:underline"
+              >
+            Regístrate aquí
+            </Link>
+          </p>
+          </div>
         </div>
       </div>
     </div>
