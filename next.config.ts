857d2e067109ba370c8e3bf046bb1be3e31bb956
GonzaloBouso <gonzalobouso002@gmail.com>
2026-02-21 06:35:50 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,13 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development", // Solo se activa en producción
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
-  // Aquí puedes agregar tus opciones de Next.js si las necesitas
+  // Añadimos esto para resolver el conflicto con Turbopack
+  // ya que el plugin de PWA necesita Webpack para funcionar.
+   turbopack: {}, 
 };
 
 export default withPWA(nextConfig);
