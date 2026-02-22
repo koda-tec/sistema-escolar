7ded156f93ee861219542ae7d59d8339a2129c11
Mili3312 <143722261+Mili3312@users.noreply.github.com>
2026-02-22 00:21:33 -0300

    fix

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -142,9 +142,13 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
                 <Link onClick={() => setSidebarOpen(false)} href="/dashboard/admin/alumnos" className={getLinkStyle('/dashboard/admin/alumnos')}>
                   <span className="text-lg">👥</span> Gestión Alumnos
                 </Link>
-                <Link onClick={() => setSidebarOpen(false)} href="/preceptor/libretas" className={getLinkStyle('/dasboard/preceptor/libretas')}>
-                <span className="text-lg">📄</span> Carga de Libretas
-                </Link>
+               <Link 
+  onClick={() => setSidebarOpen(false)} 
+  href="/dashboard/preceptor/libretas" 
+  className={getLinkStyle('/dashboard/preceptor/libretas')}
+>
+  <span className="text-lg">📄</span> Carga de Libretas
+</Link>
 
               </>
             )}
