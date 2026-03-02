e61dd7452653fdeaaa7ea38eb46e12cd690717ca
GonzaloBouso <gonzalobouso002@gmail.com>
2026-03-01 21:56:34 -0300

    Prueba sin PersistSession

--- src/app/utils/supabase/client.ts
+++ src/app/utils/supabase/client.ts
@@ -6,7 +6,6 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
      {
       auth: {
-        persistSession: true, // Obligatorio para PWA
         autoRefreshToken: true,
         detectSessionInUrl: true
       }
