3cd98608dcb3034a135bf107555141b1f30537f7
GonzaloBouso <gonzalobouso002@gmail.com>
2026-03-01 22:07:10 -0300

    Add persistSession

--- src/app/utils/supabase/client.ts
+++ src/app/utils/supabase/client.ts
@@ -6,6 +6,7 @@ export function createClient() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
      {
       auth: {
+        persistSession: true,
         autoRefreshToken: true,
         detectSessionInUrl: true
       }
